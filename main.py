34cbd7d7c655a6734678668e1154ceb0aa425d23
Royden <jaydendane2@gmail.com>
2021-02-08 16:06:25 +0200

    Updated the files

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_chuck_norris_jokes():
+
+    api_url = "https://api.chucknorris.io/jokes/random"
+    response = requests.get(api_url).json()
+
+    img = "<img src=" + response['icon_url'] + " alt='Norris image'>"
+
+    return "<strong>Random joke from chuck norris: </strong>" + response['value'] +img
+
+
+
